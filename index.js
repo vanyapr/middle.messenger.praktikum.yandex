ef28e0c3cea9a98b9e2df639c89c4ef2eccd8e62
Ivan Proskuriakov <snowflax@ya.ru>
2021-10-02 23:17:48 +0300

    feat: Добавить Nodemon и файл .nvmrc

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const PORT = 3000;
 
 // Объявили экземпляр экспресса
 const app = express();
+
+app.use('/', express.static(`${__dirname}/build`))
+
+app.listen(PORT, () => {
+    console.log('App listening at 3000');
+});
